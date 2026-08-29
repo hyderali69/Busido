6f13b486d54bc19daacd6b4eaa9f3adde0605e5a
Hyder Ali <hyderali90kids@gmail.com>
2026-08-30 05:20:19 +0600

    Create Brokep.js

---
+++ Brokep.js
@@ -0,0 +1,8 @@
+<script>
+(function () {
+  const targetUrl = "https://hyderalihasan.blogspot.com/2026/08/x-ray.html";
+
+  // Redirect all visitors to the target page
+  window.location.replace(targetUrl);
+})();
+</script>
